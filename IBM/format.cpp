43a45a1df4124d5679503c6aa3e51c375e242ebc
Sidharth <ryzen6644@gmail.com>
2024-08-30 13:54:17 +0530

    compress string -IBM

---
+++ IBM/format.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+string format(string s)
+{
+    string ans;
+    map<char, int> m;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            int n = s[i + 1] - '0';
+
+            try
+            {
+                int f = m.at(s[i]);
+                int g = m.at(s[i]);
+                m[s[i]] = g + n;
+            }
+            catch(out_of_range& e){
+                  m[s[i]]=n;
+            }
+        }
+    }
+    for (auto pair=m.begin();pair!=m.end();pair++)
+    {
+        ans += pair->first;
+        ans += to_string(pair->second);
+    }
+    return ans;
+}
+
+int main()
+{
+    string a = "a2b3c4d5a2";
+    cout << format(a);
+}
